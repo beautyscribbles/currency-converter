0973847d82120bef77d215ed56fde470629a2130
flh6 <talk2florencehanson@gmail.com>
2023-05-17 08:32:15 -0700

    added conversion rate for Naira

--- centra/convert.cpp
+++ centra/convert.cpp
@@ -10,11 +10,14 @@ using namespace std;
 
 int main() {
 
-	float a,b,c,d;
+	float a,b,c,d,e,f;
 	cout <<"hello"<<endl;
 	cout<<"press 1 to convert dollar"<<endl;
 	cout <<"press 2 to convert pounds "<<endl;
 	cout<<"press 3 to convert Euros" <<endl;
+	cout<<"press 4 to convert Yen" <<endl;
+	cout<<"press 5 to convert Naira" <<endl;
+
 	cin>>a;
 	cout<<"enter amount to be converted"<<endl;
 	cin>>b;
@@ -27,8 +30,14 @@ int main() {
 	else if(a==2){
 		  d=b*89.43;  
 		    
-		    cout<<"your amount in rupee="<<d<<endl;  
-		        
+		    cout<<"your amount in pounds="<<d<<endl;  
+	if (a==3){
+                e=b*54.79;
+                cout<<"your amount in Euros="<<e<<endl;
+        }
+        else if (a==5){
+	        f=b*786.43;
+	        cout<<"your amount in Naira="<<f<<endl;	
 		        
 	}
 	    return 0;
